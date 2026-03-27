2e30e516a53442f7e99290e896a5f1a9b440cbfd
tvs-vin <bradgaw007@gmail.com>
2026-03-27 15:06:51 -0400

    Main basic file settup using fabrics modding templete

---
+++ src/main/java/tvs/xaeros/sync/XaerossyncDataGenerator.java
@@ -0,0 +1,11 @@
+package tvs.xaeros.sync;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class XaerossyncDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
